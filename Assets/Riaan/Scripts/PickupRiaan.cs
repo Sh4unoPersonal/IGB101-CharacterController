fcfa02734c1eaa7843711b5b887e310467df13c9
Shaun <82790020+Sh4unoPersonal@users.noreply.github.com>
2026-05-29 16:44:56 +1000

    Riaan some Scripts

---
+++ Assets/Riaan/Scripts/PickupRiaan.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PickupRiaan : MonoBehaviour
+{
+    GameManagerRiaan gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerRiaan>();
+    }    
+    private void OnTriggerEnter(Collider otherObJect)
+    {
+        if(otherObJect.transform.tag == "Player")
+        {
+            gameManager.currentPickups += 1;
+            Destroy(this.gameObject);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
